89dcaf5fa684854f9da7a37bdbd80622ddcd0eb2
Brian Hackett <bhackett1024@gmail.com>
2025-08-13 13:29:49 -0700

    Add error dialog when an error is detected

--- app/lib/replay/MessageHandlerInterface.ts
+++ app/lib/replay/MessageHandlerInterface.ts
@@ -2,21 +2,34 @@
 
 type Compute<T> = { [K in keyof T]: T[K] } & unknown;
 
+interface DetectedErrorNetwork {
+  kind: "network";
+  time: string;
+  url: string;
+  responseStatus: number;
+}
+
+export type DetectedError = DetectedErrorNetwork;
+
 export type MessageHandlerRequestMap = {
-  'recording-data': {
+  "recording-data": {
     payload: unknown;
     response: ArrayBufferLike;
   };
-  'mouse-data': {
+  "mouse-data": {
     payload: { x: number; y: number };
     response: unknown;
+  };
+  "get-detected-errors": {
+    payload: unknown;
+    response: DetectedError[];
   };
 };
 
 export type MessageHandlerRequest = {
   [K in keyof MessageHandlerRequestMap]: Compute<
-    { request: K } & (undefined extends MessageHandlerRequestMap[K]['payload']
-      ? { payload?: MessageHandlerRequestMap[K]['payload'] }
-      : { payload: MessageHandlerRequestMap[K]['payload'] })
+    { request: K } & (undefined extends MessageHandlerRequestMap[K]["payload"]
+      ? { payload?: MessageHandlerRequestMap[K]["payload"] }
+      : { payload: MessageHandlerRequestMap[K]["payload"] })
   >;
 }[keyof MessageHandlerRequestMap];
