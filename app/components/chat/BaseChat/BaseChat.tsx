3577080fd153720bd813848c899d4e7482481388
Strider Wilson <strider.wilson@replay.io>
2025-09-15 10:42:41 -0400

    Update broken dreams banner

--- app/components/chat/BaseChat/BaseChat.tsx
+++ app/components/chat/BaseChat/BaseChat.tsx
@@ -27,7 +27,6 @@ import { userStore } from '~/lib/stores/userAuth';
 import type { ChatMessageParams } from '~/components/chat/ChatComponent/components/ChatImplementer/ChatImplementer';
 import { mobileNavStore } from '~/lib/stores/mobileNav';
 import { useLayoutWidths } from '~/lib/hooks/useLayoutWidths';
-import BrokenDreamsBanner from '../BrokenDreamsBanner';
 
 export const TEXTAREA_MIN_HEIGHT = 76;
 
@@ -206,7 +205,6 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
             })}
             style={!isSmallViewport && showWorkbench ? { width: `${chatWidth}px` } : { width: '100%' }}
           >
-            {!chatStarted && <BrokenDreamsBanner />}
             {!chatStarted && (
               <>
                 <IntroSection />
